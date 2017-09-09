b2f58076492652eeb4a4b5e37ead37fb252c59f3
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2017-09-09 13:14:56 -0700

    Fixed pg_user for Ubuntu 16.04

--- libraries/ic_rails_helper.rb
+++ libraries/ic_rails_helper.rb
@@ -7,7 +7,11 @@ module IcRails
 
     def postgres_is_running?
       cmd = case node['platform']
-            when 'ubuntu'; "invoke-rc.d postgresql status | grep main"
+            when 'ubuntu';
+              case node['platform_version']
+              when '16.04'; "systemctl status postgresql | grep 'Active: active'"
+              else "invoke-rc.d postgresql status | grep main"
+              end
             when 'centos'; "service postgresql status | grep 'active (running)'"
             else raise "Unknown platform: #{node['platform']}"
             end
